b6f651906af97b77064b46353dac07a94ef19c6b
Pierre Noël <petersg83@gmail.com>
2021-01-27 17:52:41 +0100

    update tests

--- packages/strapi-plugin-i18n/services/locales.js
+++ packages/strapi-plugin-i18n/services/locales.js
@@ -1,34 +1,14 @@
 'use strict';
 
-const find = async (...args) => {
-  const locales = await strapi.query('locale', 'i18n').find(...args);
+const find = async (...args) => strapi.query('locale', 'i18n').find(...args);
 
-  return locales;
-};
+const findById = async id => strapi.query('locale', 'i18n').findOne({ id });
 
-const findById = async id => {
-  const locales = await strapi.query('locale', 'i18n').findOne({ id });
+const findByCode = async code => strapi.query('locale', 'i18n').findOne({ code });
 
-  return locales;
-};
+const create = async locale => strapi.query('locale', 'i18n').create(locale);
 
-const findByCode = async code => {
-  const locales = await strapi.query('locale', 'i18n').findOne({ code });
-
-  return locales;
-};
-
-const create = async locale => {
-  const locales = await strapi.query('locale', 'i18n').create(locale);
-
-  return locales;
-};
-
-const update = async (params, updates) => {
-  const locales = await strapi.query('locale', 'i18n').update(params, updates);
-
-  return locales;
-};
+const update = async (params, updates) => strapi.query('locale', 'i18n').update(params, updates);
 
 module.exports = {
   find,
